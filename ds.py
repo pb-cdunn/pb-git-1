e5fed4626996a72bcedc00841c5f4f4a522af569
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-10-02 09:28:28 -0700

    sync

--- ds.py
+++ ds.py
@@ -7,19 +7,23 @@ import sys
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
-def sync_repo(conf):
-    log.info(conf)
-def run(args):
-    logging.getLogger().setLevel(logging.INFO)
+def init(args):
+    logging.getLogger().setLevel(logging.DEBUG)
     if args.verbose:
         logging.getLogger().setLevel(logging.NOTSET)
     group = open(args.group_file).read()
     group = eval(group) # N.B. code injection
+    log.info(args.func)
     log.info(args)
     log.info(repr(group))
     if args.reformat_group_file:
         with open(args.group_file, 'w') as ifs:
             ifs.write(pprint.pformat(group) + '\n')
+    return group
+def sync_repo(conf):
+    log.info(conf)
+def sync(args):
+    group = init(args)
     for repo, repo_conf in group['repos'].iteritems():
         sync_repo(repo_conf)
 def main(argv):
@@ -33,7 +37,10 @@ def main(argv):
     parser.add_argument('-r', '--reformat-group-file',
             action='store_true',
             help='Canonicalize the group-file. Then proceed.')
+    subparsers = parser.add_subparsers()
+    p = subparsers.add_parser('sync')
+    p.set_defaults(func=sync)
     args = parser.parse_args(argv[1:])
-    run(args)
+    args.func(args)
 
 main(sys.argv)
