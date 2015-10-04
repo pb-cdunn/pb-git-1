9e88ef40c2a194596b673d9bec69647a2f1e8038
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-10-04 16:03:04 -0700

    Improved logging (but messy)

--- pb_git/cmds.py
+++ pb_git/cmds.py
@@ -15,26 +15,29 @@ import subprocess
 import sys
 
 log = logging.getLogger(__name__)
+info_mod = logging.INFO+2
+info_sys = logging.INFO+1
+info_basic = logging.INFO+0
 cd_depth = 0
 
 @contextmanager
 def cd(newdir):
     global cd_depth
     prevdir = os.getcwd()
-    log.debug("[{}]cd '{}' from '{}'".format(cd_depth, newdir, prevdir))
+    log.info("[{}]cd '{}' from '{}'".format(cd_depth, newdir, prevdir))
     os.chdir(os.path.expanduser(newdir))
     cd_depth += 1
     try:
         yield
     finally:
         cd_depth -= 1
-        log.debug("[{}]cd '{}' back from '{}'".format(cd_depth, prevdir, newdir))
+        log.info("[{}]cd '{}' back from '{}'".format(cd_depth, prevdir, newdir))
         os.chdir(prevdir)
 
 def system(call, checked=True):
     """Raise IOError on failure if checked.
     """
-    log.info(call)
+    log.log(info_sys, call)
     rc = os.system(call)
     if rc and checked:
         raise IOError('{rc} <- {call!r}'.format(rc=rc, call=call))
@@ -44,7 +47,7 @@ def capture(call):
     """Return stdout, stderr.
     Raise IOError on failure.
     """
-    log.info('`{}`'.format(call))
+    log.log(info_sys, '`{}`'.format(call))
     # return subprocess.check_output(shlex.split(call))
     # Ugh! p4 commands often write to stderr when there is no error,
     # so we should trap that too. Why does anybody like p4?
@@ -59,18 +62,43 @@ def capture(call):
     return out, err
 
 def rename(old, new):
-    log.info('Moving "{}" to "{}"'.format(old, new))
+    log.log(info_sys, 'Moving "{}" to "{}"'.format(old, new))
     os.rename(old, new)
 
+def init_argparse(parser):
+    """Add our basic arguments for an ArgumentParser.
+    """
+    parser.add_argument('-d', '--directory',
+            default='.',
+            help='Directory of submodules')
+    parser.add_argument('-v', '--verbosity',
+            default=1, type=int,
+            help='0=>only errors/warnings; 1=>modifications; 2=>syscalls; 3=>info; 4=>debug')
+
 def init(args):
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    if args.verbose:
-        logging.getLogger().setLevel(logging.NOTSET)
+    fmt = '[%(levelname)s] %(message)s'
+    fmt_debug = '[%(levelname)s](%(pathname)s:%(lineno)s)\n\t%(message)s'
+    levels = {
+            0: (logging.WARNING, fmt),
+            1: (info_mod, fmt),
+            2: (info_sys, fmt),
+            3: (info_basic, fmt),
+            4: (logging.NOTSET, fmt_debug),
+    }
+    lvl, fmt = levels[args.verbosity]
+    logging.addLevelName(info_mod, 'MOD')
+    logging.addLevelName(info_sys, 'SYS')
+    logging.addLevelName(info_basic, 'INFO')
+    fmtr = logging.Formatter(fmt=fmt)
+    hdlr = logging.StreamHandler(sys.stderr)
+    hdlr.setFormatter(fmtr)
+    root = logging.getLogger()
+    root.addHandler(hdlr)
+    root.setLevel(lvl)
 
 def mkdirs(d):
     if not os.path.isdir(d):
-        log.info('mkdir -p {}'.format(d))
+        log.log(info_sys, 'mkdir -p {}'.format(d))
         os.makedirs(d)
 
 def write_repo_config(fp, cfg, section='general'):
@@ -97,28 +125,40 @@ def read_modules():
     """
     repos = dict()
     for fn in glob.glob('*.ini'):
-        log.info(fn)
+        log.log(info_basic, 'Found "{}"'.format(fn))
         cfg = read_repo_config(open(fn))
-        log.debug(cfg)
+        log.debug("{!r} -> {!r}".format(fn, cfg))
         name = fn[:-4]
         repos[name] = cfg
     return repos
 
 def checkout_repo(conf):
-    log.info(conf)
     d = conf['path']
+    log.debug('checkout_repo at {!r}'.format(d))
+    modified = False
     if not os.path.exists(d):
         parent = os.path.dirname(os.path.abspath(d))
         mkdirs(parent)
         with cd(parent):
             system('git clone {} {}'.format(conf['url'], conf['path']))
+            modified = True
     sha1 = conf['sha1']
-    with cd(d):
-        try:
-            system('git checkout -q {}'.format(sha1))
-        except Exception:
-            system('git fetch origin')
-            system('git checkout -q {}'.format(sha1))
+    checkout_cmd = 'git -C {} checkout {}'.format(d, sha1)
+    try:
+        out, err = capture(checkout_cmd)
+    except Exception as e:
+        log.debug('SHA1 not found. Fetching.', exc_info=True)
+        out, err = system('git -C {} fetch origin'.format(d))
+        modified = True
+        out, err = capture(checkout_cmd)
+    if 'Previous' in err or modified:
+        log.log(info_mod, '{}\n{}'.format(
+            checkout_cmd, err.strip()))
+    else:
+        log.debug(err.strip())
+    if out:
+        # This seems to be always empty, but I am not positive.
+        log.debug(out.strip())
 
 def checkout(args):
     init(args)
@@ -142,7 +182,7 @@ def prepare(args):
                 sha1new, errs = capture('git rev-parse HEAD')
                 if errs: log.debug(errs)
                 sha1new = sha1new.strip()
-                log.debug('{} {} {}'.format(sha1new, name, path))
+                log.debug('Preparing {} {} {}'.format(sha1new, name, path))
                 if sha1new == cfg['sha1']:
                     continue
                 sha1old = cfg['sha1']
@@ -150,6 +190,7 @@ def prepare(args):
             assert os.path.exists(name + '.ini')
             prepared = name + '.ini.bak'
             with open(prepared, 'w') as fp:
+                log.log(info_mod, 'Writing to {!r}'.format(prepared))
                 write_repo_config(fp, cfg)
 
 def submit(args):
@@ -166,7 +207,7 @@ def submit(args):
     mout.write('bug #{}\n'.format(args.bug))
     mout.write('\n')
     if args.dry_run:
-        system_perm = log.info
+        system_perm = functools.partial(log.log, info_mod)
     else:
         system_perm = system
     with cd(args.directory):
@@ -187,7 +228,9 @@ def submit(args):
         n = 0
         for fnnew in glob.glob('*.ini.bak'):
             fnold = fnnew[:-4]
-            if not system('diff -qw {} {}'.format(fnnew, fnold), checked=False):
+            try:
+                capture('diff -qw {} {}'.format(fnnew, fnold), checked=False)
+            except IOError:
                 system_perm('rm -f {}'.format(fnnew))
                 continue
             with open(fnold) as fp:
