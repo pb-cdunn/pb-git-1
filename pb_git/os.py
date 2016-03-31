53de0ceebc7dc28ca92232160bff37151ce80db8
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-03-31 10:45:33 -0700

    Check mirror with timeout
    Also, let user warnings show more code.

---
+++ pb_git/os.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+import os
+
+_timeout_exists = None
+
+def system(call, timeout=None):
+    """With timeout, if available.
+    """
+    global _timeout_exists
+    if _timeout_exists is None:
+        rc = os.system('timeout 1 true')
+        _timeout_exists = not rc
+    if timeout and _timeout_exists:
+        call = 'timeout {} {}'.format(timeout, call)
+    print call
+    return os.system(call)
